113ea85b11b9c92a8729049170154f7afecb5830
phamduclong142312-cpu <phamduclong142312@gmail.com>
2026-01-15 11:26:44 +0700

    Update esp32transmitter.ino

--- esp32transmitter.ino
+++ esp32transmitter.ino
@@ -1,58 +1,172 @@
+#include "WiFi.h"
 #include <esp_now.h>
-#include <WiFi.h>
-#include <SimpleKalmanFilter.h>
-#define JX1 35
-#define JY1 34
-#define JX2 33
-#define JY2 32
-SimpleKalmanFilter bo_locX2(25, 25, 0.05);
-SimpleKalmanFilter bo_locY2(25, 25, 0.05);
-uint8_t receiverAddress[] = {0xcc, 0x7b, 0x5c, 0x25, 0xbf, 0x20}; // Địa chỉ MAC của bộ nhận
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
-typedef struct struct_message {
-    int joyX1 = 0 ;
-    int joyY1 = 1;
-    int joyX2 = 2;
-    int joyY2 = 3;
+#define SCREEN_WIDTH 128  // định nghĩa chiều x màn oled
+#define SCREEN_HEIGHT 64  // định nghĩa chiều y màn oled
+#define OLED_ADDR 0x3C
+
+Adafruit_SSD1306 oled(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+uint8_t broadcastAddress[] = {0x28, 0x37, 0x2F, 0x86, 0x8A, 0x94};
+
+#define potT 15 // throttle
+#define potS 16 // steering
+#define potM 17 // max speed
+#define potTr 18 // trim steering
+#define button 41
+
+typedef struct struct_message{
+  int throttle;
+  int steering;
+  int max_speed;
+  int trim;
+  bool state;
 } struct_message;
 
-struct_message controlData;
+struct_message myData; 
 
-void setup() {
-  Serial.begin(9600);
-  WiFi.mode(WIFI_STA); //Cho phép esp này kết nối với esp thu
-  
-   if (esp_now_init() != ESP_OK) {
-        Serial.println("Error initializing ESP-NOW");
-        return; // Thoát nếu có lỗi
-    }
+typedef struct struct_message2{
+  int DfC;
+  int DfR;
+  int DfL;
+  int SL;
+  int SR;
+} struct_message2;
 
-  esp_now_peer_info_t peerInfo;
-  memcpy(peerInfo.peer_addr, receiverAddress, 6);
-  //receiverAddress: mảng có kích thước 6 byte chứa địa chỉ của Esp thu 
-  //Hàm memcpy: sao chép 6 byte của mảng receiverAddress 
-  peerInfo.channel = 0; //Đặt kênh là 0 để 
-  peerInfo.encrypt = false;
+struct_message2 DataReceived; 
 
-  if (esp_now_add_peer(&peerInfo) != ESP_OK) {
-        Serial.println("Failed to add peer");
-        return; // Thoát nếu có lỗi
-    }
+esp_now_peer_info_t peerInfo;
+
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  Serial.print("\r\nLast Packet Send Status:\t");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
 }
 
-void loop() {
-  controlData.joyX1 = joyX1 + 1;
-  controlData.joyY1 = joyY1 + 1;
-  controlData.joyX2 = joyX2 + 1;
-  controlData.joyY2 = joyY2 + 1;
-  Serial.print("joyX1 :");
-  Serial.println(controlData.joyX1);
-  Serial.print("joyY1 :");
-  Serial.println(controlData.joyY1);
-  Serial.print("joyX2 :");
-  Serial.println(controlData.joyX2);
-  Serial.print("joyY2 :");
-  Serial.println(controlData.joyY2);
-  esp_now_send(receiverAddress, (uint8_t *) &controlData, sizeof(controlData));
-  delay(100); // Giảm độ trễ để tránh việc gửi quá nhiều gói tin
+void setup(){
+  pinMode(button, INPUT_PULLUP);
+  Serial.begin(115200);
+  WiFi.mode(WIFI_MODE_STA);
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  esp_now_register_send_cb(OnDataSent);
+
+  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+  peerInfo.channel = 0;  
+  peerInfo.encrypt = false;
+
+  if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer");
+    return;
+  }
+
+  Wire.begin(5, 4);
+  if (!oled.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR)) {
+    Serial.println("Khong tim thay OLED");
+    while (true);
+  }
+  oled.setTextColor(SSD1306_WHITE); //pixel hiện sẽ có màu trắng
+  oled.clearDisplay(); // xóa hết màn hình
+}
+
+int AVGanalogRead(int pot){
+  long sum = 0;
+  const int samp = 64;
+  for(int i=0; i < samp; i++){
+    sum += analogRead(pot);
+    delayMicroseconds(200);
+  }
+  return sum/samp;
+}
+
+void THROTTLE(){
+  int value = AVGanalogRead(potT);
+  myData.throttle = constrain(map(value, 0, 4095, -255, 255),-255,255);
+}
+
+void STEERING(){
+  int value1 = AVGanalogRead(potS);
+  myData.steering = constrain(map(value1, 0, 4095, 0 ,180),0,180);
+}
+
+void MAX_SPEED(){
+  int value2 = AVGanalogRead(potM);
+  myData.max_speed = constrain(map(value2, 0, 4095, 0, 255),0,255);
+}
+
+void TRIM_SERVO(){
+  int value3 = AVGanalogRead(potTr);
+  myData.trim = constrain(map(value3, 0, 4095, 0, 180),0,180); 
+}
+
+int x=0;
+void BAOGAY(){
+  oled.setTextSize(1);
+  oled.setCursor(x,47);
+  oled.print("Bao gay vcl");
+  x+=2;
+  if(x>SCREEN_WIDTH) x=0;
+}
+
+void loop(){
+  oled.clearDisplay();
+  oled.setTextSize(1); 
+  myData.state = digitalRead(button);
+
+  if(myData.state == HIGH){
+    THROTTLE();
+    oled.setCursor(3,3);
+    oled.print("THROTTLE: ");
+    oled.print(myData.throttle);
+
+    STEERING();
+    oled.setCursor(3,14);
+    oled.print("STEERING ANGLE: ");
+    oled.print(myData.steering);
+
+    MAX_SPEED();
+    oled.setCursor(3,25);
+    oled.print("MAX SPEED: ");
+    oled.print(myData.max_speed);
+
+  TRIM_SERVO();
+  oled.setCursor(3,36);
+  oled.print("TRIM SERVO: ");
+  oled.print(myData.trim);
+  }
+  else{
+    oled.setCursor(1,1);
+    oled.print("Mode: Automotive");
+
+    oled.setCursor(1,11);
+    oled.print("TT: ");
+    oled.print(DataReceived.DfC);
+
+    oled.setCursor(1,21);
+    oled.print("TR: ");
+    oled.print(DataReceived.DfR);
+
+    oled.setCursor(1,31);
+    oled.print("TL: ");
+    oled.print(DataReceived.DfL);
+
+    oled.setCursor(1,41);
+    oled.print("SL: ");
+    oled.print(DataReceived.SL);
+
+    oled.setCursor(1,51);
+    oled.print("SR: ");
+    oled.print(DataReceived.SR);
+
+  }
+  //BAOGAY();
+
+  oled.display();
+
+  esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
 }
